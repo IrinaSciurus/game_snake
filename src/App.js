3d91a0ab39644e03598fed2e6a4032b98a50a313
IrinaSciurus <47363975+IrinaSciurus@users.noreply.github.com>
2020-09-30 12:22:34 +0300

    Add code
    Add code

--- src/App.js
+++ src/App.js
@@ -9,7 +9,6 @@ const  getRandomCoordinates = () =>{
     let y = Math.floor((Math.random()*(max-min+1)+min)/2)*2;
     return [x,y]
 }
-
 function App() {
   const [snakeDots, setSnakeDot] = useState([[0,0],[2,0]]);
   const [food, setFood] = useState(getRandomCoordinates());
@@ -67,12 +66,51 @@ function App() {
      useEffect( () => {
             let head = snakeDots[snakeDots.length-1];
             if(head[0]>=100 || head[1]>=100 || head[0] < 0 || head[1] < 0){
-                alert(`Game Over! Snake length is ${snakeDots.length}`);
+                gameOver();
             }
-         return () => {
-
-         }
       });
+
+    useEffect( () => {
+         let snake = [...snakeDots];
+         let head = snake[snake.length-1];
+         snake.pop();
+         snake.forEach(dot => {
+             if (head[0] == dot[0] && head [1] == dot[1]) {
+                 gameOver();
+             }
+         });
+     });
+
+    useEffect( () => {
+        let head = snakeDots[snakeDots.length-1];
+        // let food = food;
+        if(head[0] == food[0] && head[1] == food[1]){
+            setFood(getRandomCoordinates());
+            largestSnake();
+            increaseSpeed();
+        }
+    });
+
+    const largestSnake = () => {
+        let newSnake = [...snakeDots];
+        newSnake.unshift([])
+        setSnakeDot(newSnake);
+    }
+
+    const increaseSpeed = () => {
+        if(speed > 10){
+            setSpeed(speed-10);
+        }
+    }
+
+     const gameOver = () =>{
+         alert(`Game Over! Snake length is ${snakeDots.length}`);
+         setSnakeDot([[0,0],[2,0]]);
+         setFood(getRandomCoordinates());
+         setDirection('RIGHT');
+         setSpeed(200);
+     }
+
 
   return (
     <div className="gameArea">
