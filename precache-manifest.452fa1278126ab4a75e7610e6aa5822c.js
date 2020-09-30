de3931e066ae7df4156907046718077f8f997bd2
IrinaSciurus <47363975+IrinaSciurus@users.noreply.github.com>
2020-09-30 12:27:22 +0300

    Updates

---
+++ precache-manifest.452fa1278126ab4a75e7610e6aa5822c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d6783f460568f1d40220cfec1c26f56",
+    "url": "/game_snake/index.html"
+  },
+  {
+    "revision": "43acaaae30b56bf52e7c",
+    "url": "/game_snake/static/css/main.8f838b3f.chunk.css"
+  },
+  {
+    "revision": "8a06b54dd88d69b68e85",
+    "url": "/game_snake/static/js/2.10bf40e9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/game_snake/static/js/2.10bf40e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43acaaae30b56bf52e7c",
+    "url": "/game_snake/static/js/main.d537f6e2.chunk.js"
+  },
+  {
+    "revision": "5f8ca42abe86d88f5d60",
+    "url": "/game_snake/static/js/runtime-main.ab8d7b39.js"
+  }
+]);
